43c80b8516592eedb44e8cedd22982ddb9d613a5
simumu87 <vodhkdtorrl@gmail.com>
2025-04-10 01:28:43 +0900

    Delete ElvKeyStyle directory

--- ElvKeyStyle/config.lua
+++
@@ -1,9 +0,0 @@
-local ElvKeyStyle = LibStub("AceAddon-3.0"):GetAddon("ElvKeyStyle")
-
-ElvKeyStyle.defaults = {
-    profile = {
-        font = LibStub("LibSharedMedia-3.0"):GetDefault("font"),
-        size = 12,
-        color = { r = 1, g = 1, b = 1 },
-    }
-}
