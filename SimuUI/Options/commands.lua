cdebe63bb7c4beda8b71ae6f2cb3e9d5d4da7657
simumu87 <vodhkdtorrl@gmail.com>
2025-04-11 17:07:42 +0900

    Delete SimuUI directory

--- SimuUI/Options/commands.lua
+++
@@ -1,3 +0,0 @@
-local SimuUI = LibStub("AceAddon-3.0"):GetAddon("SimuUI")
-SimuUI:RegisterChatCommand("simu", function() InterfaceOptionsFrame_OpenToCategory(SimuUI.optionsFrame) end)
-SimuUI:RegisterChatCommand("simuui", function() InterfaceOptionsFrame_OpenToCategory(SimuUI.optionsFrame) end)
--- SimuUI/Options/options.lua
+++
@@ -1,8 +0,0 @@
-local SimuUI = select(2, ...).SimuUI
-
-local options = CreateFrame("Frame", "SimuUIOptionsPanel")
-options.name = "SimuUI"
-
--- 설정 UI 정의는 여기
-
-SimuUI.optionsPanel = options
