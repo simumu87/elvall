1d31ba689e5f8c17d1d559b91fb15d5861c71a5b
simumu87 <vodhkdtorrl@gmail.com>
2025-04-12 19:42:58 +0900

    Delete SimuUI directory

--- SimuUI/Options/commands.lua
+++
@@ -1,5 +0,0 @@
-SLASH_SIMUUI1 = "/simu"
-SlashCmdList["SIMUUI"] = function()
-    InterfaceOptionsFrame_OpenToCategory("SimuUI")
-    InterfaceOptionsFrame_OpenToCategory("SimuUI") -- Twice for bug workaround
-end
